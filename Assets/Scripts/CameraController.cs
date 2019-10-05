f4e98582222e7c59a749db2ab11f0d41ecf49e1a
jondalnas <jdudal11@gmail.com>
2019-10-05 12:56:34 +0200

    Added player character

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+	public Transform player;
+	public Vector3 offset = new Vector3(0, 0, -10);
+
+	
+	public void updateCamera(Vector2 playerVel) {
+		if (!player) return;
+
+		transform.position = player.position + offset + (Vector3) playerVel * Time.fixedDeltaTime;
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+	Rigidbody2D rb;
+	CameraController cc;
+	public float movementSpeed = 2;
+
+	void Start() {
+		rb = GetComponent<Rigidbody2D>();
+		cc = Camera.main.GetComponent<CameraController>();
+
+	}
+	
+	void FixedUpdate() {
+		if (!rb) return;
+
+		float move = Input.GetAxis("Horizontal") * movementSpeed;
+
+		rb.velocity = move * Vector2.right + rb.velocity.y * Vector2.up;
+
+		cc.updateCamera(rb.velocity);
+	}
+}
