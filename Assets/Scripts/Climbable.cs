9ad50c69195c9067fd7f9e87cccd3aae76a67c10
Jonas Jensen <jdudal11@gmail.com>
2021-02-05 01:34:04 +0100

    Post LD commit

--- Assets/Scripts/Climbable.cs
+++ Assets/Scripts/Climbable.cs
@@ -3,16 +3,35 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Climbable : MonoBehaviour {
-	private BoxCollider2D collider;
-
 	void Start() {
-		collider = GetComponent<BoxCollider2D>();
-	}
+		BoxCollider2D collider = GetComponent<BoxCollider2D>();
 
-	//Returns where the pos is on the collider
-	private float posToLocalScale(Vector2 pos) {
-		collider.bounds.ClosestPoint(pos);
+		//Create egde that the player can hold on to
+		EdgeCollider2D[] edges = new EdgeCollider2D[4];
+		for (int i = 0; i < edges.Length; i++) {
+			edges[i] = gameObject.AddComponent(typeof(EdgeCollider2D)) as EdgeCollider2D;
+		}
 
-		return 0;
+		float minX = -collider.size.x / 2.0f + collider.offset.x;
+		float minY = -collider.size.y / 2.0f + collider.offset.y;
+		float maxX = collider.size.x / 2.0f + collider.offset.x;
+		float maxY = collider.size.y / 2.0f + collider.offset.y;
+
+		//Set edges positions
+		edges[0].points = new Vector2[] { new Vector2(minX, maxY),
+										  new Vector2(minX, minY) };
+
+		edges[1].points = new Vector2[] { new Vector2(minX, minY),
+										  new Vector2(maxX, minY) };
+
+		edges[2].points = new Vector2[] { new Vector2(maxX, minY),
+										  new Vector2(maxX, maxY) };
+
+		edges[3].points = new Vector2[] { new Vector2(maxX, maxY),
+										  new Vector2(minX, maxY) };
+
+		foreach (EdgeCollider2D edge in edges) {
+			edge.enabled = false;
+		}
 	}
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,25 +7,23 @@ public class PlayerController : MonoBehaviour {
 
 	private Transform holding;
 	private float holdingLocation;
-    private EdgeCollider2D holdingCollider;
-    private Vector2[] holdingPoses = new Vector2[2];
-    private float armLength;
-    private Transform arm;
-    private float edgeLength;
-    private float distArmToFoot;
-    private bool climbing;
-    private Vector2 holdingDir;
-    private Vector2[] holdingDirs;
-    EdgeCollider2D[] edges;
-    private int currentEdge;
+	private EdgeCollider2D holdingCollider;
+	private Vector2[] holdingPoses = new Vector2[2];
+	private Transform arm;
+	private float edgeLength;
+	private bool climbing;
+	private Vector2 holdingDir;
+	private Vector2[] holdingDirs;
+	EdgeCollider2D[] edges;
+	private int currentEdge;
 
-    private HingeJoint2D[] arms;
-    private float armDist;
-    public float changeArmDistance = 0.25f;
-    private int currentArm = -1;
-    public GameObject playerSpritePrefab;
+	private HingeJoint2D[] arms;
+	private float armDist;
+	public float changeArmDistance = 0.25f;
+	private int currentArm = -1;
+	public GameObject playerSpritePrefab;
 
-    public float movementSpeed = 2;
+	public float movementSpeed = 2;
 	public float climbSpeed = 2;
 	public float jumpSpeed = 5;
 
@@ -35,13 +33,10 @@ public class PlayerController : MonoBehaviour {
 		rb = GetComponent<Rigidbody2D>();
 		cc = Camera.main.GetComponent<CameraController>();
 		foot = transform.Find("Foot");
+		arm = transform.Find("Sprites").Find("Player Torso").Find("Player Arm Front");
 
-        arm = transform.Find("Sprites").Find("Player Torso").Find("Player Arm Front");
-        armLength = arm.lossyScale.y;
-        distArmToFoot = (arm.position - foot.position).magnitude;
-
-        arms = new HingeJoint2D[] { transform.Find("Sprites").Find("Player Torso").Find("Player Arm Front").GetComponents<HingeJoint2D>()[1] , transform.Find("Sprites").Find("Player Torso").Find("Player Arm Back").GetComponents<HingeJoint2D>()[1] };
-    }
+		arms = new HingeJoint2D[] { transform.Find("Sprites").Find("Player Torso").Find("Player Arm Front").GetComponents<HingeJoint2D>()[1] , transform.Find("Sprites").Find("Player Torso").Find("Player Arm Back").GetComponents<HingeJoint2D>()[1] };
+	}
 	
 	void FixedUpdate() {
 		//Don't run if player doesn't have a rigidbody
@@ -54,85 +49,98 @@ public class PlayerController : MonoBehaviour {
 			//If the player releases the hold button, then stop holding
 			if (!climbing) {
 				holding = null;
-                //transform.position += (Vector3) (-holdingDir * (transform.lossyScale * (-holdingDir * GetComponent<BoxCollider2D>().size - GetComponent<BoxCollider2D>().offset)).magnitude * 1/2f);
-                transform.position = transform.Find("Sprites").Find("Player Torso").position;
-                if (holdingDir.y > 0) transform.position += (holdingDir.y * GetComponent<BoxCollider2D>().size.y * 2) * Vector3.up;
+				transform.position = transform.Find("Sprites").Find("Player Torso").position;
+				if (holdingDir.y > 0) transform.position += (holdingDir.y * GetComponent<BoxCollider2D>().size.y * 2) * Vector3.up;
 
-                Destroy(transform.Find("Sprites").gameObject);
-                Transform sprites = Instantiate(playerSpritePrefab, transform).transform;
-                sprites.gameObject.name = "Sprites";
+				Destroy(transform.Find("Sprites").gameObject);
+				Transform sprites = Instantiate(playerSpritePrefab, transform).transform;
+				sprites.gameObject.name = "Sprites";
 
-                holdingLocation = 0;
+				holdingLocation = 0;
 
-                arm = sprites.Find("Player Torso").Find("Player Arm Front");
-                arms = new HingeJoint2D[] { sprites.Find("Player Torso").Find("Player Arm Front").GetComponents<HingeJoint2D>()[1], sprites.Find("Player Torso").Find("Player Arm Back").GetComponents<HingeJoint2D>()[1] };
-                
-                return;
+				arm = sprites.Find("Player Torso").Find("Player Arm Front");
+				arms = new HingeJoint2D[] { sprites.Find("Player Torso").Find("Player Arm Front").GetComponents<HingeJoint2D>()[1], sprites.Find("Player Torso").Find("Player Arm Back").GetComponents<HingeJoint2D>()[1] };
+				
+				return;
 			}
 
 			//Move player so she holds on
 			float climb = Input.GetAxis("Vertical") * (climbSpeed * Time.fixedDeltaTime / edgeLength);
 
 			holdingLocation += climb;
-            holdingLocation = Mathf.Clamp01(holdingLocation);
+			holdingLocation = Mathf.Clamp01(holdingLocation);
 
-            if (holdingLocation >= 1) {
-                holdingLocation = 0;
-                currentEdge--;
-                if (currentEdge < 0) currentEdge = 3;
+			if (holdingLocation >= 1) {
+				holdingLocation = 0;
+				currentEdge--;
+				if (currentEdge < 0) currentEdge = 3;
 
-                holdingCollider = edges[currentEdge];
-                holdingDir = holdingDirs[currentEdge];
+				holdingCollider = edges[currentEdge];
+				holdingDir = holdingDirs[currentEdge];
 
-                holdingPoses[0] = holdingCollider.transform.TransformPoint(holdingCollider.points[0]);
-                holdingPoses[1] = holdingCollider.transform.TransformPoint(holdingCollider.points[1]);
+				holdingPoses[0] = holdingCollider.transform.TransformPoint(holdingCollider.points[0]);
+				holdingPoses[1] = holdingCollider.transform.TransformPoint(holdingCollider.points[1]);
 
-                edgeLength = (holdingPoses[0] - holdingPoses[1]).magnitude;
-            } else if (holdingLocation <= 0) {
-                holdingLocation = 1;
-                currentEdge++;
-                if (currentEdge > 3) currentEdge = 0;
+				edgeLength = (holdingPoses[0] - holdingPoses[1]).magnitude;
+			} else if (holdingLocation <= 0) {
+				holdingLocation = 1;
+				currentEdge++;
+				if (currentEdge > 3) currentEdge = 0;
 
-                holdingCollider = edges[currentEdge];
-                holdingDir = holdingDirs[currentEdge];
+				holdingCollider = edges[currentEdge];
+				holdingDir = holdingDirs[currentEdge];
 
-                holdingPoses[0] = holdingCollider.transform.TransformPoint(holdingCollider.points[0]);
-                holdingPoses[1] = holdingCollider.transform.TransformPoint(holdingCollider.points[1]);
+				holdingPoses[0] = holdingCollider.transform.TransformPoint(holdingCollider.points[0]);
+				holdingPoses[1] = holdingCollider.transform.TransformPoint(holdingCollider.points[1]);
 
-                edgeLength = (holdingPoses[0] - holdingPoses[1]).magnitude;
-            }
+				edgeLength = (holdingPoses[0] - holdingPoses[1]).magnitude;
+			}
 
 			Vector2 newPos = holdingPosition();
 
-            //Check if characters hands are going to end up inside something, if not, then move
-            Collider2D[] cols;
-            if ((cols = Physics2D.OverlapCircleAll(newPos, 0.1f, ~(LayerMask.GetMask("Player")))).Length < 2) {
-                transform.position = newPos;
+			//Check if characters hands are going to end up inside something, if not, then move
+			Collider2D[] cols;
+			if ((cols = Physics2D.OverlapCircleAll(newPos, 0.1f, ~(LayerMask.GetMask("Player")))).Length < 2) {
+				transform.position = newPos;
 
-                if (currentArm == -1) {
-                    arms[0].connectedAnchor = newPos;
-                    arms[1].connectedAnchor = newPos;
+				if (currentArm == -1) {
+					arms[0].connectedAnchor = newPos;
+					arms[1].connectedAnchor = newPos;
 
-                    currentArm = 0;
-                }
+					currentArm = 0;
+				}
 
-                arms[currentArm].connectedAnchor += (newPos - arms[currentArm].connectedAnchor) * 0.25f;
-                armDist += Mathf.Abs(climb) * edgeLength;
-                if (armDist > changeArmDistance) {
-                    armDist = 0;
-                    currentArm++;
-                    if (currentArm >= arms.Length) currentArm = 0;
-                }
-            } else {
-                /*foreach (Collider2D col in cols) {
-                    if (!col.CompareTag("Climbable")) break;
-                    else if (col.transform != holding) {
-                        hold((BoxCollider2D) col, col.transform);
-                    }
-                }*/
+				arms[currentArm].connectedAnchor += (newPos - arms[currentArm].connectedAnchor) * 0.25f;
+				armDist += Mathf.Abs(climb) * edgeLength;
+				if (armDist > changeArmDistance) {
+					armDist = 0;
+					currentArm++;
+					if (currentArm >= arms.Length) currentArm = 0;
+				}
+			} else {
+				float bestDot = -1f;
+				Collider2D bestCol = null;
 
-                holdingLocation -= climb;
-            }
+				foreach (Collider2D col in cols) {
+					if (!col.CompareTag("Climbable")) continue;
+					//Check if angle between checking collider and moving direction is smaller than the current 
+
+					Vector3 colDir = col.transform.position - transform.position;
+					Vector3 moveDir = (Vector3) newPos - transform.position;
+					float dot = Vector3.Dot(colDir, moveDir);
+
+					if (dot > bestDot) {
+						bestDot = dot;
+						bestCol = col;
+					} 
+				}
+
+				if (holding != bestCol.transform) {
+					hold((BoxCollider2D) bestCol, bestCol.transform, transform.position);
+
+					holdingLocation -= climb;
+				}
+			}
 
 		} else {
 			rb.simulated = true;
@@ -144,7 +152,7 @@ public class PlayerController : MonoBehaviour {
 			if (Input.GetButtonDown("Jump")) {
 				//Check if player is on ground
 				RaycastHit2D hit = Physics2D.Raycast(foot.position + Vector3.down * 0.1f, Vector2.down, distanceToGroundBias);
-                
+				
 				if (hit) {
 					vertical += jumpSpeed;
 				}
@@ -153,102 +161,122 @@ public class PlayerController : MonoBehaviour {
 			//Update velocity
 			rb.velocity = move * Vector2.right + (rb.velocity.y + vertical) * Vector2.up;
 
-            RaycastHit2D[] hits = new RaycastHit2D[2];
-            if (Physics2D.RaycastAll(transform.position, rb.velocity * Vector2.right, Mathf.Abs(rb.velocity.x) * Time.fixedDeltaTime, ~(LayerMask.GetMask("Player"))).Length > 0) rb.velocity = Vector2.up * rb.velocity;
-            if (hits.Length != 0 && hits[0].transform) Debug.Log(hits[0].transform.name);
-        }
-	}
-
-    void Update() {
-        if (Input.GetButtonDown("Climb")) climbing = true;
-        if (Input.GetButtonUp("Climb")) climbing = false;
-
-        //Debug.DrawLine(transform.position, transform.position - transform.right, Color.red);
-    }
-
-    Vector2 holdingPosition() {
-        return (holdingPoses[0]) * holdingLocation + (holdingPoses[1]) * (1f - holdingLocation);
-    }
-
-    private void ragdoll() {
-        Transform torso = transform.Find("Sprites").Find("Player Torso");
-
-        foreach (Rigidbody2D r in torso.GetComponentsInChildren<Rigidbody2D>()) {
-            r.simulated = true;
-        }
-
-        foreach (BoxCollider2D c in torso.GetComponentsInChildren<BoxCollider2D>()) {
-            c.enabled = true;
-        }
-
-        torso.GetComponent<Rigidbody2D>().simulated = true;
-        torso.GetComponent<BoxCollider2D>().enabled = true;
-
-        currentArm = -1;
-    }
-
-    private void deRagdoll() {
-        Transform torso = transform.Find("Sprites").Find("Player Torso");
-
-        foreach (Rigidbody2D r in torso.GetComponentsInChildren<Rigidbody2D>()) {
-            r.simulated = false;
-        }
-
-        foreach (BoxCollider2D c in torso.GetComponentsInChildren<BoxCollider2D>()) {
-            c.enabled = false;
-        }
-
-        torso.GetComponent<Rigidbody2D>().simulated = false;
-        torso.GetComponent<BoxCollider2D>().enabled = false;
-    }
-
-    void OnCollisionEnter2D(Collision2D collision) {
-        //Check if cllision is climbable and that the player is climbing
-        if (collision.gameObject.CompareTag("Climbable")) {
-			if (climbing) {
-                hold((BoxCollider2D) collision.collider, collision.transform);
-
-                ragdoll();
-            }
+			//If there isn't any other solution
+			/*RaycastHit2D[] hits = new RaycastHit2D[2];
+			if (Physics2D.RaycastAll(transform.position, rb.velocity * Vector2.right, Mathf.Abs(rb.velocity.x) * Time.fixedDeltaTime, ~(LayerMask.GetMask("Player"))).Length > 0) {
+				rb.velocity = Vector2.up * rb.velocity;
+			}*/
 		}
 	}
 
-    private void hold(BoxCollider2D col, Transform trans) {
-        holding = trans;
+	void Update() {
+		if (Input.GetButtonDown("Climb")) climbing = true;
+		if (Input.GetButtonUp("Climb")) climbing = false;
+	}
 
-        edges = trans.GetComponents<EdgeCollider2D>();
+	Vector2 holdingPosition() {
+		return (holdingPoses[0]) * holdingLocation + (holdingPoses[1]) * (1f - holdingLocation);
+	}
 
-        holdingDirs = new Vector2[] { col.transform.right, col.transform.up, -col.transform.right, -col.transform.up };
+	private void ragdoll() {
+		Transform torso = transform.Find("Sprites").Find("Player Torso");
 
-        RaycastHit2D[] hit = new RaycastHit2D[3];
-        col.size *= 0.99f;
-        if (col.Cast(-col.transform.right, hit, 1f) > 0) {
-            holdingDir = col.transform.right;
-            currentEdge = 0;
-        } else if (col.Cast(col.transform.right, hit, 1f) > 0) {
-            holdingDir = -col.transform.right;
-            currentEdge = 2;
-        } else if (col.Cast(-col.transform.up, hit, 1f) > 0) {
-            holdingDir = col.transform.up;
-            currentEdge = 1;
-        } else if (col.Cast(col.transform.up, hit, 1f) > 0) {
-            holdingDir = -col.transform.up;
-            currentEdge = 3;
-        }
+		foreach (Rigidbody2D r in torso.GetComponentsInChildren<Rigidbody2D>()) {
+			r.simulated = true;
+		}
 
-        col.size /= 0.99f;
+		foreach (BoxCollider2D c in torso.GetComponentsInChildren<BoxCollider2D>()) {
+			c.enabled = true;
+		}
 
-        holdingCollider = edges[currentEdge];
+		torso.GetComponent<Rigidbody2D>().simulated = true;
+		torso.GetComponent<BoxCollider2D>().enabled = true;
 
-        holdingPoses[0] = holdingCollider.transform.TransformPoint(holdingCollider.points[0]);
-        holdingPoses[1] = holdingCollider.transform.TransformPoint(holdingCollider.points[1]);
+		currentArm = -1;
+	}
 
-        
-        Debug.Log(holdingDir);
-        
+	private void deRagdoll() {
+		Transform torso = transform.Find("Sprites").Find("Player Torso");
 
-        edgeLength = (holdingPoses[0] - holdingPoses[1]).magnitude;
+		foreach (Rigidbody2D r in torso.GetComponentsInChildren<Rigidbody2D>()) {
+			r.simulated = false;
+		}
 
-        holdingLocation = (holdingPoses[1] - col.ClosestPoint(arm.position)).magnitude / edgeLength;
-    }
+		foreach (BoxCollider2D c in torso.GetComponentsInChildren<BoxCollider2D>()) {
+			c.enabled = false;
+		}
+
+		torso.GetComponent<Rigidbody2D>().simulated = false;
+		torso.GetComponent<BoxCollider2D>().enabled = false;
+	}
+
+	void OnCollisionStay2D(Collision2D collision) {
+		if (holding) return;
+
+		//Check if cllision is climbable and that the player is climbing
+		if (collision.gameObject.CompareTag("Climbable")) {
+			if (climbing) {
+				hold((BoxCollider2D) collision.collider, collision.transform, arm.position);
+
+				ragdoll();
+			}
+		}
+	}
+
+	private void hold(BoxCollider2D col, Transform trans, Vector2 connectionPoint) {
+		holding = trans;
+
+		edges = trans.GetComponents<EdgeCollider2D>();
+
+		holdingDirs = new Vector2[] { col.transform.right, col.transform.up, -col.transform.right, -col.transform.up };
+
+		/*RaycastHit2D[] hit = new RaycastHit2D[3];
+		col.size *= 0.9f;
+		if (col.Cast(-col.transform.right, hit, 1f) > 0) {
+			holdingDir = col.transform.right;
+			currentEdge = 0;
+		} else if (col.Cast(col.transform.right, hit, 1f) > 0) {
+			holdingDir = -col.transform.right;
+			currentEdge = 2;
+		} else if (col.Cast(-col.transform.up, hit, 1f) > 0) {
+			holdingDir = col.transform.up;
+			currentEdge = 1;
+		} else if (col.Cast(col.transform.up, hit, 1f) > 0) {
+			holdingDir = -col.transform.up;
+			currentEdge = 3;
+		}
+
+		col.size /= 0.9f;*/
+
+		//Check what direction player is on
+		float closestDist = Mathf.Infinity;
+		for (int i = 0; i < edges.Length; i++) {
+			EdgeCollider2D c = edges[i];
+
+			Vector2[] poses = {c.transform.TransformPoint(c.points[0]), c.transform.TransformPoint(c.points[1])};
+
+			//dist=sqrt(-d⁴ + 2*d²*r0² + 2*d²*r1² + 2*r0²*r1² - r1⁴ - r0⁴)/(2*d)
+			float r0 = (poses[0] - connectionPoint).sqrMagnitude;
+			float r1 = (poses[1] - connectionPoint).sqrMagnitude;
+			float d = (poses[0] - poses[1]).sqrMagnitude;
+			float dist = Mathf.Sqrt((2*d*r0+2*d*r1+2*r0*r1-r1*r1-r0*r0-d*d)/(4f*d));
+
+			if (dist != dist) Debug.Log(r0 + ", " + r1 + ", " + d);
+			if (closestDist > dist) {
+				closestDist = dist;
+
+				currentEdge = i;
+
+				holdingPoses = poses;
+			}
+		}
+
+		holdingDir = holdingDirs[currentEdge];
+
+		holdingCollider = edges[currentEdge];
+
+		edgeLength = (holdingPoses[0] - holdingPoses[1]).magnitude;
+
+		holdingLocation = (holdingPoses[1] - col.ClosestPoint(connectionPoint)).magnitude / edgeLength;
+	}
 }
